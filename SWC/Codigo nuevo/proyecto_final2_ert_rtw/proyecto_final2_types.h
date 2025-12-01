7141f79952eca2f5fde257d41a7dca94709ac7f5
Juanchofis <juan.felix@edu.uag.mx>
2025-12-01 15:02:23 -0600

    agregué nuevos códigos

---
+++ SWC/Codigo nuevo/proyecto_final2_ert_rtw/proyecto_final2_types.h
@@ -0,0 +1,32 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: proyecto_final2_types.h
+ *
+ * Code generated for Simulink model 'proyecto_final2'.
+ *
+ * Model version                  : 1.42
+ * Simulink Coder version         : 25.2 (R2025b) 28-Jul-2025
+ * C/C++ source code generated on : Mon Dec  1 14:57:24 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex-M
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef proyecto_final2_types_h_
+#define proyecto_final2_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_proyecto_final2_T RT_MODEL_proyecto_final2_T;
+
+#endif                                 /* proyecto_final2_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
